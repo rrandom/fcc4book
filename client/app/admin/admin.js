f95d2daa5ce4b1bae7d725db85a3fe03b114b203
Rrandom <emanonhere@gmail.com>
2015-09-28 21:28:18 +0800

    initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('fcc4bookApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('fcc4bookApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
